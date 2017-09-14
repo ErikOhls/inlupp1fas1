a8933aef2e0c332d8a11cd3c798013ba6b8b714a
Erik <Ohls2nd@gmail.com>
2017-09-14 17:57:16 +0200

    halvklart insert

--- database/tree.c
+++ database/tree.c
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "tree.h"
 
 /// Define struct tree in your .c file not here! (why?)
@@ -42,6 +43,11 @@ node_t *node_new(K key, T elem)
   new_node->elem = elem;
   return new_node;
 }
+
+void print_specific(tree_t *tree)
+{
+  printf("%s", tree->top->right->key);
+}
 /// Remove a tree along with all T elements.
 /// Note, if T is a pointer, elements will not
 /// be removed. 
@@ -49,7 +55,7 @@ node_t *node_new(K key, T elem)
 /// \returns: empty tree
 void tree_delete(tree_t *tree)
 {
-
+  // Ska alltså köra free() på SAMTLIGA noder.
 }
 
 /// Get the size of the tree 
@@ -68,15 +74,46 @@ int tree_depth(tree_t *tree)
   return 0;
 }
 
+node_t *tree_insert_helper(node_t *cursor, K key, T elem)
+{
+  puts("top of helper\n");
+  if(cursor == NULL)
+    {
+      puts("reached null\n");
+      cursor = node_new(key, elem);
+      return cursor;
+    }
+  if(strcmp(key, cursor->key) < 0)
+    {
+      puts("in left\n");
+      cursor->left = tree_insert_helper(cursor->left, key, elem);
+    }
+  if(strcmp(key, cursor->key) > 0)
+    {
+      puts("in right\n");
+      cursor->right = tree_insert_helper(cursor->right, key, elem);
+    }
+  if(strcmp(key, cursor->key) == 0)
+    {
+      puts("in equal\n");
+      return node_new(key, elem);
+    }
+  puts("reached (supposedly) impossible state\n");
+  return node_new(key, elem);
+}
 /// Insert element into the tree. Returns false if the key is already used.
 ///
 /// \param tree pointer to the tree
 /// \param key the key of element to be appended
 /// \param elem the element 
 /// \returns: true if successful, else false
-bool tree_insert(tree_t *tree, K key, T elem)
+bool tree_insert(tree_t *tree, K key, T elem) // Ej helt funktionell än
 {
-  return true;
+  bool success = false;
+  node_t *cursor = tree->top;
+  puts("calling insert_helper\n");
+  tree_insert_helper(cursor, key, elem);
+  return success;
 }
 
 /// Checks whether a key is used in a tree
@@ -118,5 +155,9 @@ int main(void)
   t->top->left = node_new("ghi", 3);
   t->top->right->right = node_new("klm", 4);
   t->top->right->left = node_new("opq", 5);
+
+  tree_insert(t, "xxx", 6);
+  print_specific(t);
+  
   return 0;
 }
